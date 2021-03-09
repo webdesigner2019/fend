52ed611e6c881bad987424bb796169223e573289
Elaine <lainerodriguez06@gmail.com>
2021-03-08 18:05:22 -0800

    removed href that lets page scrolling to a section in app.js, instead added addEventListener to scroll to a certain section instead of href as per rubrics.

--- Elaine_LandingPageProject/js/app.js
+++ Elaine_LandingPageProject/js/app.js
@@ -27,18 +27,40 @@ const navCreate = () => {
     let nav = '';
     //  loop on all four sections 
     sections.forEach(section => {
+        
         const sectionID = section.id;
         const sectionDataNav = section.dataset.nav;
 
-        nav += `
-            <li id="${sectionID}Link">
-                <a class="menu__link" href="#${sectionID}">${sectionDataNav}</a>
-            </li>
-        `;
+        // create a list item for the unordered list
+        const sectionListItem = document.createElement('li');
+        // create an anchor element for the list item above
+        const sectionListItemAnchor = document.createElement('a');
+
+        // assign the listItem an ID for targeting
+        sectionListItem.id = `${sectionID}Link`;
+
+        // add the class to the anchor element
+        sectionListItemAnchor.classList.add('menu__link');
+        // add the text to display for the anchor element
+        sectionListItemAnchor.innerText = sectionDataNav;
+
+        // add an event listener to the anchor element that was just created
+        sectionListItemAnchor.addEventListener('click', (event) => {
+            // prevent the current behavior of the element
+            event.preventDefault();
+            // grab the position of the top part of the section
+            const sectionTop = document.getElementById(sectionID).getBoundingClientRect().top;
+            // scroll to that position ( adding the current scroll position as the above value is relative to the position of the user
+            window.scrollTo({
+                top: window.scrollY + sectionTop,
+                behavior: "smooth"
+            });
+        });
+        // append the anchor element to the list item
+        sectionListItem.appendChild(sectionListItemAnchor);
+        // append the list item to the navbar
+        navi.appendChild(sectionListItem);
     });
-    
-    //append all elements to the nav bar
-    navi.innerHTML = nav;
 };
 navCreate();
 
